485c25082dd05bdaf1ebfd8100a738adaa4e5e82
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-05-10 18:48:27 -0400

    test: publish FW packages into local E2E package registry

--- tests/legacy-cli/e2e/setup/010-local-publish.ts
+++ tests/legacy-cli/e2e/setup/010-local-publish.ts
@@ -1,9 +1,22 @@
+import { packages } from '../../../../lib/packages';
 import { getGlobalVariable } from '../utils/env';
-import { npm } from '../utils/process';
+import { npm, silentNpm } from '../utils/process';
 import { isPrereleaseCli } from '../utils/project';
+
+async function publishLocal(specifier: string, tags: string[], registry: string): Promise<void> {
+  const { stdout: stdoutPack1 } = await silentNpm(
+    'pack',
+    specifier,
+    '--registry=https://registry.npmjs.org',
+  );
+  for (const tag of tags) {
+    await npm('publish', stdoutPack1.trim(), `--registry=${registry}`, `--tag=${tag}`);
+  }
+}
 
 export default async function () {
   const testRegistry = getGlobalVariable('package-registry');
+  const prerelease = isPrereleaseCli();
   await npm(
     'run',
     'admin',
@@ -13,6 +26,31 @@ export default async function () {
     '--no-branchCheck',
     `--registry=${testRegistry}`,
     '--tag',
-    isPrereleaseCli() ? 'next' : 'latest',
+    prerelease ? 'next' : 'latest',
   );
+
+  const cliVersionParts = packages['@angular/cli'].version.split('.');
+  const version = prerelease ? 'next' : `${cliVersionParts[0]}.${cliVersionParts[1]}`;
+  const angularPackages = [
+    'animations',
+    'common',
+    'compiler',
+    'core',
+    'forms',
+    'platform-browser',
+    'platform-browser-dynamic',
+    'router',
+  ];
+  const ltsVersions = ['8', '9', '10', '11'];
+  for (const angularPackage of angularPackages) {
+    await publishLocal(`@angular/${angularPackage}@${version}`, ['latest', 'next'], testRegistry);
+
+    for (const ltsVersion of ltsVersions) {
+      await publishLocal(
+        `@angular/${angularPackage}@${ltsVersion}`,
+        [`lts-${ltsVersion}`],
+        testRegistry,
+      );
+    }
+  }
 }
