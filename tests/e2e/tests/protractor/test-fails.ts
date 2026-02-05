191e182370bae25dad5dc0e336d677863c9b1ba2
Charles Lyding <19598772+clydin@users.noreply.github.com>
2026-02-05 14:32:52 -0500

    test: remove global Protractor/E2E setup from E2E utilities
    Since most E2E tests have been migrated away from Protractor, this change removes the automatic generation of the 'private-e2e' schematic and its associated configuration from the global 'prepareProjectForE2e' and 'useCIChrome' utilities. The setup is now explicitly handled within the 'browsers' and `protractor/test-fails` tests which still requires it, resulting in cleaner and faster initialization for the majority of the E2E suite.

--- tests/e2e/tests/protractor/test-fails.ts
+++ tests/e2e/tests/protractor/test-fails.ts
@@ -1,11 +1,12 @@
-import { execAndCaptureError } from '../../utils/process';
+import { execAndCaptureError, ng } from '../../utils/process';
 import { updateJsonFile } from '../../utils/project';
 
 export default async function () {
-  // Revert the `private-protractor` builder name back to the previous `protractor`.
+  // Setup `protractor` builder.
   await updateJsonFile('angular.json', (config) => {
-    config.projects['test-project'].architect['e2e'].builder =
-      '@angular-devkit/build-angular:protractor';
+    config.projects['test-project'].architect['e2e'] = {
+      builder: '@angular-devkit/build-angular:protractor',
+    };
   });
 
   const error = await execAndCaptureError('ng', ['e2e']);
